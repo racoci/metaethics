a927ff391f468c9f3f59c26e2aba701e32bcdd9d
Lucas Racoci <racoci.0@gmail.com>
2026-09-04 10:46:58 -0300

    proof(pragmatics): Prove Performative Contradiction and Transcendental Bridge in Isabelle and Lean

--- UNC/Pragmatics.lean
+++ UNC/Pragmatics.lean
@@ -20,4 +20,23 @@ variable (argue_assert : ∀ a phi w, ExecArgue a phi w → ExecAssert a phi w)
 variable (argue_presuppose_ver : ∀ a phi w, ExecArgue a phi w → Presuppose a N_ver w)
 variable (argue_presuppose_eq : ∀ a phi w, ExecArgue a phi w → Presuppose a N_eq w)
 
+-- 3. Deontic Output under the Discourse Perspective
+variable (deontic_output_ver : ∀ a phi w, ExecArgue a phi w → (∀ v, B p_discourse w v → N_ver v))
+variable (deontic_output_eq : ∀ a phi w, ExecArgue a phi w → (∀ v, B p_discourse w v → N_eq v))
+
+-- 4. Performative Contradiction Predicate
+def PC (a : Agent) (chi : World → Prop) (w : World) : Prop :=
+  ExecAssert a chi w ∧ ∃ psi : World → Prop, Presuppose a psi w ∧ (∀ v, chi v → ¬ psi v)
+
+-- 5. Transcendental Bridge Theorem (The Deduction)
+theorem transcendental_bridge (a : Agent) (w : World) (h_argue : ExecArgue a (fun w => ¬ N_ver w) w) :
+  PC ExecAssert Presuppose a (fun w => ¬ N_ver w) w := by
+  have h_assert := argue_assert a (fun w => ¬ N_ver w) w h_argue
+  have h_presup := argue_presuppose_ver a (fun w => ¬ N_ver w) w h_argue
+  apply And.intro h_assert
+  use N_ver
+  apply And.intro h_presup
+  intro v h_not_ver
+  exact h_not_ver
+
 end Pragmatics
--- UNC/UNC_Pragmatics.thy
+++ UNC/UNC_Pragmatics.thy
@@ -16,4 +16,30 @@ axiomatization where
   argue_presuppose_ver: "\<lfloor>ExecArgue a \<phi> \<^bold>\<rightarrow> Presuppose a N_ver\<rfloor>" and
   argue_presuppose_eq: "\<lfloor>ExecArgue a \<phi> \<^bold>\<rightarrow> Presuppose a N_eq\<rfloor>"
 
+(* 3. Deontic Output under the Discourse Perspective *)
+axiomatization where
+  deontic_output_ver: "\<lfloor>ExecArgue a \<phi> \<^bold>\<rightarrow> \<^bold>O\<^sub>p_discourse N_ver\<rfloor>" and
+  deontic_output_eq: "\<lfloor>ExecArgue a \<phi> \<^bold>\<rightarrow> \<^bold>O\<^sub>p_discourse N_eq\<rfloor>"
+
+(* 4. Performative Contradiction Predicate *)
+definition PC :: "a \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" where
+  "PC a \<chi> \<equiv> \<lambda>w. (ExecAssert a \<chi>) w \<and> (\<exists>\<psi>. (Presuppose a \<psi>) w \<and> \<lfloor>\<chi> \<^bold>\<rightarrow> \<^bold>\<not>\<psi>\<rfloor>)"
+
+(* 5. Transcendental Bridge Theorem (The Deduction) *)
+theorem transcendental_bridge: "\<lfloor>ExecArgue a (\<^bold>\<not>N_ver) \<^bold>\<rightarrow> PC a (\<^bold>\<not>N_ver)\<rfloor>"
+proof (intro allI)
+  fix w
+  assume "ExecArgue a (\<^bold>\<not>N_ver) w"
+  then have h_assert: "ExecAssert a (\<^bold>\<not>N_ver) w"
+    using argue_assert by blast
+  have h_presup: "Presuppose a N_ver w"
+    using argue_presuppose_ver `ExecArgue a (\<^bold>\<not>N_ver) w` by blast
+  have h_impl: "\<lfloor>\<^bold>\<not>N_ver \<^bold>\<rightarrow> \<^bold>\<not>N_ver\<rfloor>"
+    by simp
+  from h_presup h_impl have "\<exists>\<psi>. Presuppose a \<psi> w \<and> \<lfloor>\<^bold>\<not>N_ver \<^bold>\<rightarrow> \<^bold>\<not>\<psi>\<rfloor>"
+    by blast
+  with h_assert show "(PC a (\<^bold>\<not>N_ver)) w"
+    unfolding PC_def by simp
+qed
+
 end
